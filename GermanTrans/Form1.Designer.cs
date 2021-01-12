d1541ea7f9663c2937a91d7427b6bac2d275115c
leonhong <leonhong@8700K>
2021-01-13 04:09:32 +0900

    초기 커밋.

---
+++ GermanTrans/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GermanTrans
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.edt_src = new System.Windows.Forms.TextBox();
+            this.btn_trans = new System.Windows.Forms.Button();
+            this.edt_res_google = new System.Windows.Forms.TextBox();
+            this.sta_res_google = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // edt_src
+            // 
+            this.edt_src.Location = new System.Drawing.Point(34, 49);
+            this.edt_src.Name = "edt_src";
+            this.edt_src.Size = new System.Drawing.Size(328, 21);
+            this.edt_src.TabIndex = 0;
+            // 
+            // btn_trans
+            // 
+            this.btn_trans.Location = new System.Drawing.Point(389, 42);
+            this.btn_trans.Name = "btn_trans";
+            this.btn_trans.Size = new System.Drawing.Size(78, 32);
+            this.btn_trans.TabIndex = 1;
+            this.btn_trans.Text = "Translation";
+            this.btn_trans.UseVisualStyleBackColor = true;
+            this.btn_trans.Click += new System.EventHandler(this.btn_trans_Click);
+            // 
+            // edt_res_google
+            // 
+            this.edt_res_google.Location = new System.Drawing.Point(139, 98);
+            this.edt_res_google.Name = "edt_res_google";
+            this.edt_res_google.Size = new System.Drawing.Size(328, 21);
+            this.edt_res_google.TabIndex = 2;
+            // 
+            // sta_res_google
+            // 
+            this.sta_res_google.AutoSize = true;
+            this.sta_res_google.Location = new System.Drawing.Point(36, 101);
+            this.sta_res_google.Name = "sta_res_google";
+            this.sta_res_google.Size = new System.Drawing.Size(85, 12);
+            this.sta_res_google.TabIndex = 3;
+            this.sta_res_google.Text = "구글 번역 결과";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 150);
+            this.Controls.Add(this.sta_res_google);
+            this.Controls.Add(this.edt_res_google);
+            this.Controls.Add(this.btn_trans);
+            this.Controls.Add(this.edt_src);
+            this.Name = "Form1";
+            this.Text = "German Translation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox edt_src;
+        private System.Windows.Forms.Button btn_trans;
+        private System.Windows.Forms.TextBox edt_res_google;
+        private System.Windows.Forms.Label sta_res_google;
+    }
+}
+
---
+++ GermanTrans/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Google.Cloud.Translation.V2;
+
+namespace GermanTrans
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_trans_Click(object sender, EventArgs e)
+        {
+            TranslationClient client = TranslationClient.Create();
+            string strSrc = edt_src.Text;
+
+            var response = client.TranslateText(
+                    text: strSrc,
+                    targetLanguage: "ko",
+                    sourceLanguage: "de"  // "de":독일, "en":영어
+                    );
+
+            edt_res_google.Text = response.TranslatedText;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
